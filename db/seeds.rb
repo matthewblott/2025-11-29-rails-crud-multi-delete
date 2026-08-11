e7e3091fde71452ed3d1d83898637fcf31dbbdab
Matthew Blott <m4ttb@pm.me>
2026-08-11 16:58:45 +0100

    Updated ruby version.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 # Create 100 posts
 100.times.with_index do |i|
-    Post.create(
-        title: Faker::Lorem.sentence(word_count: 3),
-        body: Faker::Lorem.paragraph(sentence_count: 3),
-    )
-    puts "Created post #{i + 1}"
+  Post.create(
+    title: Faker::Lorem.sentence(word_count: 3),
+    body: Faker::Lorem.paragraph(sentence_count: 3),
+  )
+  puts "Created post #{i + 1}"
 end
 
