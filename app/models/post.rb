c32c90ed5f49dc029a5f7f7871fd6f1a5e73a3e4
Matthew Blott <m4ttb@pm.me>
2026-09-04 16:05:00 +0100

    Remove unnecessary turbo frame pages.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
+  # After a Post is created the contents of the partial view "posts/_post.html.erb"
+  # are appended to the html element with the id of "posts_element" of any page wherever a 
+  # stream named "posts_after_create_stream" is rendered.
   after_create_commit -> { 
     broadcast_prepend_to :posts_after_create_stream,
       target: :posts_element,
